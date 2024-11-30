989d39d2a56c7c8a2feb65ff95a13d2bea6a7d8c
Your Name <your_email@example.com>
2024-11-30 23:22:48 +0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,16 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// next.config.js
+module.exports = {
+  images: {
+    domains: [
+      'cdn.shopify.com',      // Shopify domain
+      'tse4.mm.bing.net',     // Bing domain
+      '4.bp.blogspot.com',    // Blogger domain
+    ],
+  },
+};
+
+
