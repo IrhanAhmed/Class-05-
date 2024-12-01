1b7dfd800bb09b3af8e1abad41acd4bfc540cf88
Your Name <your_email@example.com>
2024-12-01 14:14:33 +0500

    Initial commit or Update next.config and images

--- src/app/componnts/featuredProducts.tsx
+++ src/app/componnts/featuredProducts.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 const FeaturedProducts = () => {
   return (
     <div className="flex lg:flex-row flex-col justify-around">
-      {/* First Product (Local Image) */}
+      {/* First Product */}
       <div className="md:w-5/12 w-full mb-20">
         <div className="flex flex-col items-center">
           <Image
-            src="https://cdn.shopify.com/s/files/1/0554/9424/6423/files/hero-desk_2_a7af6170-3cff-4876-8b2d-043b294b4927.png?v=1654967076"
-            alt="Product Three"
+            src="/PP1.jpg"
+            alt="Product One"
             width={300}
             height={300}
           />
@@ -21,13 +21,13 @@ const FeaturedProducts = () => {
         </div>
       </div>
 
-      {/* Second Product (External Image) */}
+      {/* Second Product */}
       <div className="md:w-6/12 w-full mb-20">
         <div className="flex flex-col items-center">
           <Image
-            src="https://tse4.mm.bing.net/th?id=OIP.I8UrgvPmNZFlXuIoDRoPtAHaE8&pid=Api&P=0&h=220"
-            alt="Product Three"
-            width={300}
+            src="/PP2.png"
+            alt="Product Two"
+            width={350}
             height={300}
           />
           <p className="text-center pt-10">PKR 5,000</p>
@@ -39,14 +39,14 @@ const FeaturedProducts = () => {
         </div>
       </div>
 
-      {/* Third Product (Local Image) */}
+      {/* Third Product */}
       <div className="md:w-6/12 w-full">
         <div className="flex flex-col items-center">
           <Image
-            src="http://4.bp.blogspot.com/-xSZWi7i6pdE/TgTIzuILX1I/AAAAAAAAAEA/FfUkGFr7Hwk/s1600/cos.jpg"
+            src="/PP3.jpg"
             alt="Product Three"
-            width={300}
-            height={300}
+            width={250}
+            height={250}
           />
           <p className="text-center pt-10">PKR 5,000</p>
         </div>
